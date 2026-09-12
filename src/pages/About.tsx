1750d187b84b00754e912e163d0a55b9c81cfb68
Mathias Leleu <mathiasleleupro@gmail.com>
2026-09-12 18:23:35 +0200

    feat: add some About content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,18 @@
 export default function About() {
-    return <h1>About</h1>
+    return (
+        <main className="about-page">
+            <div>
+                <h1 className="page-main-title"> À PROPOS</h1>
+                
+                <section className="about-section">
+                    <p>Gravelya Studio — Vos idées, nos créations</p>
+                    <p>J'aime créer, tout simplement. C'est ce qui a donné naissance à Gravelya Studio.</p>
+                    <p>Ici, je grave et découpe du bois, je fais de l'impression 3D, je réalise des découpes et cartes sur-mesure, ainsi que des porte-clés en inox et autres matériaux. 
+                    Vous trouverez aussi bien des créations déjà prêtes à adopter que des pièces personnalisées selon vos envies. </p>
+                    <p>Une idée en tête, ou juste envie de découvrir mes créations ? N'hésitez pas à me contacter.</p>
+
+                </section>
+            </div>
+        </main>
+    )
 }
