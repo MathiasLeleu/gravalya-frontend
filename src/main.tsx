75abf7a06396eb162a46e92d7c477f79e57f19e2
Mathias Leleu <mathiasleleupro@gmail.com>
2026-09-13 13:57:51 +0200

    add 'scroll on top' feature

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
+import ScrollToTop from "./components/ScrollToTop";
+
 import "./components/Header/header.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
