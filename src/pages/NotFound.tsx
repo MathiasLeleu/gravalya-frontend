c3f9fd06a19f3ac2398d12a6b30ebe7ac4fea34a
Mathias Leleu <mathiasleleupro@gmail.com>
2026-09-13 18:05:33 +0200

    404 Not Found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,34 @@
+import image404Desktop from "../assets/404desktop.png";
+import image404Tablet from "../assets/404tablette.png";
+import image404Mobile from "../assets/404mobile.png";
+
 export default function NotFound() {
     return (
-        <main>
-            <h1>404</h1>
-            <p>La page que vous recherchez n'existe pas.</p>
+        <main className="not-found-page">
+            <picture>
+                <source
+                    media="(max-width: 767px)"
+                    srcSet={image404Mobile}
+                />
+
+                <source
+                    media="(max-width: 1199px)"
+                    srcSet={image404Tablet}
+                />
+
+                <img
+                    src={image404Desktop}
+                    alt="Page introuvable"
+                    className="not-found-image"
+                />
+            </picture>
+
+            <button
+                className="not-found-button"
+                onClick={() => window.location.href = "/"}
+            >
+                Retour à l'accueil
+            </button>
         </main>
-    )
+    );
 }
